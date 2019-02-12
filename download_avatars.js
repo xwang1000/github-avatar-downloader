1759a1e0696ac08cc72039ee44e7323d7e063c94
xw <23066125+xwang1000@users.noreply.github.com>
2019-02-11 23:17:44 -0800

    Add functions to display contributors.

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,52 @@
 var request = require('request');
+var fs = require('fs')
+var secrets = require('./secrets.js')
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+    .on('error', function (err) {
+      throw err
+    })
+    .on('response', function (response) {
+      console.log('Response Status Code: ', response.statusCode);
+    })
+    .pipe(fs.createWriteStream(filePath));       
+}
+
+function downloadImageByContributor(contributor) {
+  const path = '.avatars/' + contributor.login
+  downloadImageByURL(contributor['avatar_url'], `./avatars/${contributor['login']}.jpg`)
+}
+
+// Takes a contributor object and shows its properties
+function showContributorAvatarUrl(contributor) {
+  console.log(`${contributor.login}: ${contributor.avatar_url}`)
+}
+
+// Takes an array of contributor objects, show each
+function showContributors(contributors) {
+  contributors.forEach(showContributorAvatarUrl)
+  contributors.forEach(downloadImageByContributor)
+}
+
+// request url
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': secrets.GITHUB_TOKEN
+    }
+  };  
+  request(options, function(err, res, body) {
+    const data = JSON.parse(body)
+    cb(err, data);
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  if (err) {
+    throw err
+  }
+  showContributors(result)
+});
